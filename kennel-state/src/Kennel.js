d4c35942292c38dceacf7018f3b5e7d822cf0d42
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-07-11 15:04:33 -0500

    Completed advanced challenge

--- kennel-state/src/Kennel.js
+++ kennel-state/src/Kennel.js
@@ -8,19 +8,38 @@ export default class Kennel extends Component {
 
   state = {
     employees: [
-      { name: "Jessica Younker" },
-      { name: "Jordan Nelson" },
-      { name: "Zoe LeBlanc" },
-      { name: "Blaise Roberts" }
+      { id: 1, name: "Jessica Younker" },
+      { id: 2, name: "Jordan Nelson" },
+      { id: 3, name: "Zoe LeBlanc" },
+      { id: 4, name: "Blaise Roberts" }
     ],
     locations: [
-      { name: "Nashville North" },
-      { name: "Nashville South" }
+      { id: 1, name: "Nashville North" },
+      { id: 2, name: "Nashville South" }
     ],
     animals: [
-      { name: "Butch" },
-      { name: "Fido" },
-      { name: "Spike" }
+      { id: 1, name: "Doodles" },
+      { id: 2, name: "Jack" },
+      { id: 3, name: "Angus" },
+      { id: 4, name: "Henley" },
+      { id: 5, name: "Derkins" },
+      { id: 6, name: "Checkers" }
+    ],
+    owners: [
+      { id: 1, name: "Ryan Tanay" },
+      { id: 2, name: "Emma Beaton" },
+      { id: 3, name: "Dani Adkins" },
+      { id: 4, name: "Adam Oswalt" },
+      { id: 5, name: "Fletcher Bangs" },
+      { id: 6, name: "Angela Lee" }
+    ],
+    animalOwners: [
+      { ownerId: 1, animalId: 2 },
+      { ownerId: 2, animalId: 4 },
+      { ownerId: 3, animalId: 6 },
+      { ownerId: 4, animalId: 5 },
+      { ownerId: 5, animalId: 3 },
+      { ownerId: 6, animalId: 1 }
     ]
   }
 
@@ -29,7 +48,7 @@ export default class Kennel extends Component {
       <React.Fragment>
         <LocationList locations={this.state.locations} />
         <EmployeeList employees={this.state.employees} />
-        <AnimalList animals={this.state.animals} />
+        <AnimalList animals={this.state.animals} owners={this.state.owners} animalOwners={this.state.animalOwners} />
       </React.Fragment>
     );
   }
