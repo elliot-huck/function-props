efd134065f04fb40cfa0d8e98c29d42f5bb3f276
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-07-12 10:32:15 -0500

    Files are set up according to the chapter; ready to begin the exercise

--- kennel-routes/src/EmployeeList.js
+++ kennel-routes/src/EmployeeList.js
@@ -6,13 +6,13 @@ export default class EmployeeList extends Component {
     return (
       <React.Fragment>
         <h2>Employees</h2>
-        {
+        {/* {
           this.props.employees.map(employee =>
             <div>
               {employee.name}
             </div>
           )
-        }
+        } */}
       </React.Fragment>
     )
   }
