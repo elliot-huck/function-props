b59ee31b5f4519d4d71250326ef959a46e12f8da
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-07-13 15:22:56 -0500

    Added login file

--- kennel-conditional/src/ApplicationViews.js
+++ kennel-conditional/src/ApplicationViews.js
@@ -1,17 +1,28 @@
 import { Route } from 'react-router-dom'
 import React, { Component } from "react"
+
 import AnimalList from './AnimalList'
 import Animal from './Animal'
+
 import LocationList from './LocationList'
 import Location from './Location'
+
 import EmployeeList from './EmployeeList'
 import Employee from './Employee'
 
+import Login from './Login'
+
 
 export default class ApplicationViews extends Component {
+
+	// Check if credentials are in local storage
+	isAuthenticated = () => localStorage.getItem("credentials") !== null
+
 	render() {
 		return (
 			<React.Fragment>
+				<Route path="/login" component={Login} />
+				
 				<Route exact path="/" component={LocationList} />
 				<Route path="/locations/:locationId"
 					render={
@@ -20,6 +31,7 @@ export default class ApplicationViews extends Component {
 						}
 					}
 				/>
+
 				<Route exact path="/animals" component={AnimalList} />
 				<Route path="/animals/:animalId"
 					render={
@@ -28,6 +40,7 @@ export default class ApplicationViews extends Component {
 						}
 					}
 				/>
+
 				<Route exact path="/employees" component={EmployeeList} />
 				<Route path="/employees/:employeeId"
 					render={
