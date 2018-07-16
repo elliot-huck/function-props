855d9ad67b8b5a5feb888cfd38d659d67640a908
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-07-16 11:55:56 -0500

    Search bar now console logs matching employees dynamically

--- kennel-api/src/ApiController.js
+++ kennel-api/src/ApiController.js
@@ -4,5 +4,9 @@ export default {
   getAll: (table) => {
     return fetch(`http://localhost:5050/${table}`)
       .then(e => e.json())
+  },
+  searchAll: (table, text) => {
+    return fetch(`http://localhost:5050/${table}?q=${text}`)
+      .then(e => e.json())
   }
 }
